1f5b849fac2f8e68499c5fed46688c09e631d475
kawsar-uddin <kawsaruddin450@gmail.com>
2025-03-24 13:20:44 +0600

    get todos api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectToDB from "./database/db.js";
+import { Todo } from "./models/todo.model.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,22 @@ const port = process.env.port || 5000;
 app.use(express.json());
 
 connectToDB();
+
+app.get("/todos", async(req, res)=> {
+    try{
+        const result = await Todo.find()
+        res.send({
+            success: true,
+            message: "Todo lists retrieved successfully.",
+            data: result,
+        })
+    }catch{
+        res.send({
+            success: false,
+            message: "Failed to retrieve todo lists.",
+        })
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`Todo server is running at ${port}.`);
